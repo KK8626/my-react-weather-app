a8455bb377ccdf3cc821b13831716ed7b87d7bff
KK8626 <92926472+KK8626@users.noreply.github.com>
2022-03-18 15:39:01 +0100

    Added basic CSS styling

--- src/Weather.js
+++ src/Weather.js
@@ -4,19 +4,38 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <h1 className="city">New York</h1>
+      <div className="row">
+        <div className="col-9">
+          <form>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter a city"
+              autoFocus="on"
+            />
+          </form>
+        </div>
+        <div className="col-3">
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-primary w-100"
+          />
+        </div>
+      </div>
+      <h1 className="city mt-3 fw-bold">New York</h1>
       <ul>
         <li className="date">Friday 11:00</li>
         <li className="description">Mist</li>
       </ul>
-      <div className="row">
+      <div className="row mt-4">
         <div className="col-6">
           <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+            src="https://ssl.gstatic.com/onebox/weather/64/fog.png"
             alt="Mist"
           />
-          <span className="temperature">10</span>
-          <span className="unit">°C</span>
+          <span className="temperature fw-bold">10</span>
+          <span className="unit fw-bold">°C</span>
         </div>
         <div className="col-6">
           <ul>
